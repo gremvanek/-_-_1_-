fec50c777e44e3286dcf37428b11bda7d0f1c68c
gremv <gremvanek@gmail.com>
2023-12-21 20:07:48 +0500

    Дз 2 по ООП бета

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,5 +1,5 @@
 import pytest
-from src.Classes import Category, Product
+from src.classes import Category, Product
 
 
 @pytest.fixture
@@ -14,32 +14,33 @@ def test_category_2():
 
 @pytest.fixture
 def test_product_1():
-    return Product("Смартфон", "Современный смартфон", 499.99, 50)
+    return Product("Смартфон", 499.99, 50)
 
 
 @pytest.fixture
 def test_product_2():
-    return Product("Футболка", "Мужская футболка", 19.99, 100)
+    return Product("Футболка", 19.99, 100)
 
 
 def test_category_initialization(test_category_1):
-    assert test_category_1.name == "Электроника"
-    assert test_category_1.description == "Технические устройства"
-    assert test_category_1.products == []
+    assert test_category_1.get_name() == "Электроника"
+    assert test_category_1.get_description() == "Технические устройства"
+    assert test_category_1.get_products() == ''
 
 
 def test_product_initialization(test_product_1):
-    assert test_product_1.name == "Смартфон"
-    assert test_product_1.description == "Современный смартфон"
-    assert test_product_1.price == 499.99
-    assert test_product_1.quantity_in_stock == 50
+    assert test_product_1.get_name() == "Смартфон"
+    assert test_product_1.get_price() == 499.99
+    assert test_product_1.get_stock() == 50
 
 
 def test_add_product_to_category_and_count(test_category_1, test_category_2, test_product_1, test_product_2):
     test_category_1.add_product(test_product_1)
-    assert len(test_category_1.products) == 1
-    assert Category.total_products == 1
+    assert len(test_category_1.get_products().split(
+        '\n')) == 1  # Разделяем строку по символу перевода строки и проверяем длину
 
     test_category_2.add_product(test_product_2)
-    assert len(test_category_2.products) == 1
-    assert Category.total_products == 2
+    assert len(test_category_2.get_products().split('\n')) == 1  # Разделяем строку по символу перевода строки
+    # и проверяем длину
+
+    assert Category.get_total_products() == 2
