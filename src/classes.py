b97176e9eead543004788d1b0eb59c1239d050d0
gremv <gremvanek@gmail.com>
2024-01-09 19:26:02 +0500

    Дз 4 по ООП бета

--- src/classes.py
+++ src/classes.py
@@ -25,8 +25,11 @@ class Category:
         Category.total_categories += 1
 
     def add_product(self, product):
-        self.__products.append(product)
-        Category.total_products += 1
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.total_products += 1
+        else:
+            raise ValueError("Нельзя добавить в категорию объект, не являющийся продуктом")
 
     def get_name(self):
         return self.__name
@@ -35,7 +38,7 @@ class Category:
         return self.__description
 
     def get_products(self):
-        return '\n'.join(map(str, self.__products))
+        return self.__products
 
     def __str__(self):
         return f"{self.__name}, количество продуктов: {len(self.__products)} шт."
@@ -44,7 +47,7 @@ class Category:
         return len(self.__products)
 
     def __iter__(self):
-        return CategoryIterator(self.__products)
+        return iter(self.__products)
 
     @staticmethod
     def get_total_products():
@@ -82,6 +85,7 @@ class Product:
     def __str__(self):
         return f"{self.__name}, {self.__price} руб. Остаток: {self.__stock} шт."
 
+    # Переопределяем оператор сложения для продуктов одного типа
     def __add__(self, other):
         if isinstance(other, Product):
             total_price = (self.__price * self.__stock) + (other.__price * other.__stock)
@@ -92,15 +96,65 @@ class Product:
 
     @staticmethod
     def create_product(name, price, stock, category):
-        for product in category.__products:
+        for product in category.get_products():
             if product.get_name() == name:
                 # Товар с таким именем уже существует, обновляем его
                 if price > product.get_price():
                     product.price = price
-                product.stock += stock
+                product_stock = product.get_stock()
+                product_stock += stock
+                product.__stock = product_stock
                 return product
 
         # Товар с таким именем не найден, создаем новый
         new_product = Product(name, price, stock)
         category.add_product(new_product)
         return new_product
+
+
+class SmartPhone(Product):
+    def __init__(self, name, price, stock, performance, model, memory_capacity, color):
+        super().__init__(name, price, stock)
+        self.__performance = performance
+        self.__model = model
+        self.__memory_capacity = memory_capacity
+        self.__color = color
+
+    def get_performance(self):
+        return self.__performance
+
+    def get_model(self):
+        return self.__model
+
+    def get_memory_capacity(self):
+        return self.__memory_capacity
+
+    def get_color(self):
+        return self.__color
+
+    def __str__(self):
+        return f"Смартфон {self.__name}, {self.__price} руб. Остаток: {self.__stock} шт. " \
+               f"Производительность: {self.__performance}, Модель: {self.__model}, " \
+               f"Объем встроенной памяти: {self.__memory_capacity}, Цвет: {self.__color}"
+
+
+class Grass(Product):
+    def __init__(self, name, price, stock, country, sprout_period, color):
+        super().__init__(name, price, stock)
+        self.__country = country
+        self.__sprout_period = sprout_period
+        self.__color = color
+
+    def get_country(self):
+        return self.__country
+
+    def get_sprout_period(self):
+        return self.__sprout_period
+
+    def get_color(self):
+        return self.__color
+
+    def __str__(self):
+        return f"Газонная трава {self.__name}, {self.__price} руб. Остаток: {self.__stock} шт. " \
+               f"Страна-производитель: {self.__country}, Срок прорастания: {self.__sprout_period}, " \
+               f"Цвет: {self.__color}"
